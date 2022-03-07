b1b2ba479e6f0b933389e9f9cab456b292b11eba
dangquy <pxwebshop@gmail.com>
2022-03-07 14:19:43 +0700

    test scss

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,8 +10,13 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+mix.sass('resources/sass/front/style.scss', 'public/css/front')
 
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
+
+mix.options({
+    processCssUrls: false,
+})
