229b0761e1a90b1682a7ab6485c00b7b540e0bd8
congminhnguyen <minhcongnguyen11@gmail.com>
2022-04-12 11:53:43 +0700

    create component list3, list4, list5

--- resources/views/libs/component/08_list.blade.php
+++ resources/views/libs/component/08_list.blade.php
@@ -190,12 +190,153 @@ list
                     <div class="c-list2__content c-list2__content--width">
                         <p class="c-list2__txt">Featured</p>
                         <h3 class="c-list2__title">Best Price & Great Quality</h3>
-                        <p class="c-list2__price c-list2__price--discount">50% Off</p><br>
+                        <p class="c-list2__price c-list2__price--discount">-50% Off</p><br>
                         <a href="#" class="c-list2__btn">
                             View Product
                         </a>
                     </div>
                 </div>
+            </div>
+        </div>
+    </div>
+    <h1>list 3</h1>
+    <div class="c-list3">
+        <div class="c-list3__list">
+            <div class="c-list3__item">
+                <div class="c-list3__wrap">
+                    <div class="c-list3__img">
+                        <a href="#">
+                            <figure class="c-list3__image">
+                                <img src="https://templates.envytheme.com/ejon/default/assets/img/offer-products/offer-products-1.png" alt="">
+                            </figure>
+                        </a>
+                        <div class="c-list3__tag">-50%</div>
+                    </div>
+                    <div class="c-list3__content">
+                        <p class="c-list3__txt">Gadget</p>
+                        <h3 class="c-list3__name">Smart Watch</h3>
+                        <div class="c-list3__price">
+                            <p class="c-list3__promotionPrice"> 9.990.000 <sup>đ</sup></p>
+                            <p class="c-list3__cost">19.990.000 <sup>đ</sup></p>
+                        </div>
+                        <ul class="c-list3__rate">
+                            <li><i class="fa-solid fa-star"></i></li>
+                            <li><i class="fa-solid fa-star"></i></li>
+                            <li><i class="fa-solid fa-star"></i></li>
+                            <li><i class="fa-solid fa-star"></i></li>
+                            <li><i class="fa-solid fa-star"></i></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="c-list3__item">
+                <div class="c-list3__wrap">
+                    <div class="c-list3__img">
+                        <a href="#">
+                            <figure class="c-list3__image">
+                                <img src="https://templates.envytheme.com/ejon/default/assets/img/offer-products/offer-products-1.png" alt="">
+                            </figure>
+                        </a>
+                        <div class="c-list3__tag">-50%</div>
+                    </div>
+                    <div class="c-list3__content">
+                        <p class="c-list3__txt">Gadget</p>
+                        <h3 class="c-list3__name">Smart Watch</h3>
+                        <div class="c-list3__price">
+                            <p class="c-list3__promotionPrice"> 9.990.000 <sup>đ</sup></p>
+                            <p class="c-list3__cost">19.990.000 <sup>đ</sup></p>
+                        </div>
+                        <ul class="c-list3__rate">
+                            <li><i class="fa-solid fa-star"></i></li>
+                            <li><i class="fa-solid fa-star"></i></li>
+                            <li><i class="fa-solid fa-star"></i></li>
+                            <li><i class="fa-solid fa-star"></i></li>
+                            <li><i class="fa-solid fa-star"></i></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="c-list3__item">
+                <div class="c-list3__wrap">
+                    <div class="c-list3__img">
+                        <a href="#">
+                            <figure class="c-list3__image">
+                                <img src="https://templates.envytheme.com/ejon/default/assets/img/offer-products/offer-products-3.png" alt="">
+                            </figure>
+                        </a>
+                        <div class="c-list3__tag">-50%</div>
+                    </div>
+                    <div class="c-list3__content">
+                        <p class="c-list3__txt">Gadget</p>
+                        <h3 class="c-list3__name">Smart Watch</h3>
+                        <div class="c-list3__price">
+                            <p class="c-list3__promotionPrice"> 9.990.000 <sup>đ</sup></p>
+                            <p class="c-list3__cost">19.990.000 <sup>đ</sup></p>
+                        </div>
+                        <ul class="c-list3__rate">
+                            <li><i class="fa-solid fa-star"></i></li>
+                            <li><i class="fa-solid fa-star"></i></li>
+                            <li><i class="fa-solid fa-star"></i></li>
+                            <li><i class="fa-solid fa-star"></i></li>
+                            <li><i class="fa-solid fa-star"></i></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <h1>list 4</h1>
+    <div class="c-list4">
+        <div class="c-list4__list">
+            <div class="c-list4__wrap">
+                <div class="c-list4__item">
+                    <i class="fa-solid fa-truck"></i>
+                    <p class="c-list4__txt">Free Shipping Worldwide</p>
+                </div>
+            </div>
+            <div class="c-list4__wrap">
+                <div class="c-list4__item">
+                    <i class="fa-solid fa-rotate-left"></i>
+                    <p class="c-list4__txt">30 Days Money Returns</p>
+                </div>
+            </div>
+            <div class="c-list4__wrap">
+                <div class="c-list4__item">
+                    <i class="fa-solid fa-wallet"></i>
+                    <p class="c-list4__txt">100% Secure Payment</p>
+                </div>
+            </div>
+            <div class="c-list4__wrap">
+                <div class="c-list4__item">
+                    <i class="fa-solid fa-headset"></i>
+                    <p class="c-list4__txt">24/7 Customer Support</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <h1>list 5</h1>
+    <div class="c-list5">
+        <div class="c-list5__list">
+            <div class="c-list5__item">
+                <figure class="c-list5__img">
+                    <img src="https://templates.envytheme.com/ejon/default/assets/img/collection/collection-1.png" alt="">
+                </figure>
+            </div>
+            <div class="c-list5__item c-list5__item--flex">
+                <div class="c-list5__content">
+                    <div class="c-list5__txt1">New Arrival</div>
+                    <h3 class="c-list5__title">Best Gadget</h3>
+                    <div class="c-list5__txt2">Collection</div>
+                    <a href="#" class="c-btn1">
+                        <i class="fa-solid fa-cart-shopping"></i>
+                        Shop Now
+                    </a>
+                </div>
+            </div>
+            <div class="c-list5__item c-list5__item--flex">
+                <figure class="c-list5__img">
+                    <img src="https://templates.envytheme.com/ejon/default/assets/img/collection/collection-2.png" alt="">
+                </figure>
             </div>
         </div>
     </div>
--- resources/views/libs/component/12_slide.blade.php
+++ resources/views/libs/component/12_slide.blade.php
@@ -6,19 +6,19 @@ slide
 <?php /*----------------------------------------*/ ?>
 
 
-<div class="c-slide">
+<div class="c-slide1">
     <div class="l-container">
-        <div class="c-slide__content">
-            <div class="c-slide__text">
-                <p class="c-slide__bold">Big Sale Offer</p>
-                <h1 class="c-slide__title">The Drone has an Attractive Gift Free</h1>
-                <p class="c-slide__txt">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <div class="c-slide1__content">
+            <div class="c-slide1__text">
+                <p class="c-slide1__bold">Big Sale Offer</p>
+                <h1 class="c-slide1__title">The Drone has an Attractive Gift Free</h1>
+                <p class="c-slide1__txt">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                 <a href="#" class="c-btn1">
                     <i class="fa-solid fa-cart-shopping"></i>
                     Shop Now
                 </a>
             </div>
-            <figure class="c-slide__img">
+            <figure class="c-slide1__img">
                 <img src="https://templates.envytheme.com/ejon/default/assets/img/main-slider/drone.png" alt="">
             </figure>
         </div>
