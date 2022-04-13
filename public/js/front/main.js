ae02ffe1423372eaee263a9e95c8bd616e3cad81
congminhnguyen <minhcongnguyen11@gmail.com>
2022-04-13 16:17:33 +0700

    add c-header__navbar

--- public/js/front/main.js
+++ public/js/front/main.js
@@ -1,6 +1,6 @@
 jQuery(function ($) {
     'use strict';
-	
+
 	// Home Slides
 	$('.c-slide1').owlCarousel({
 		loop: true,
@@ -16,3 +16,24 @@ jQuery(function ($) {
 		],
 	});
 }(jQuery));
+
+$(document).ready(function () {
+    "use strict";
+
+    $(".c-header__nav__btn").click(function () {
+        $(".c-header__nav__menu").toggle();
+    });
+	$(".c-header__nav__btn .fa-solid").click(function () {
+		$(".c-header__nav__btn .fa-solid").toggleClass("fa-bars fa-xmark");
+	});
+    $(".c-header__nav__item").on("click", function (e) {
+        e.preventDefault();
+		$(this).find('i').toggle();
+        var submenu = $(this).children(".c-header__nav__subMenu");
+        if ($(submenu).is(":hidden")) {
+            $(submenu).slideDown(200);
+        } else {
+            $(submenu).slideUp(200);
+        }
+    });
+});
--- resources/views/libs/header.blade.php
+++ resources/views/libs/header.blade.php
@@ -9,6 +9,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Noto+Sans+Lao:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 </head>
 
 <body>
@@ -88,29 +89,119 @@
             </div>
         </div>
     </header>
+    <!-- navbar: pc -->
     <div class="c-header__sticky">
         <div class="l-container">
             <div class="c-header__navbar">
                 <ul class="c-header__menuList">
-                    <li class="c-header__menuItem"><a href="#">Home</a></li>
+                    <li class="c-header__menuItem"><a href="#" class="c-header__link">Home</a></li>
                     <li class="c-header__menuItem">
-                        <a href="#">Computer<i class="fa-solid fa-caret-down"></i></a>
+                        <a href="#" class="c-header__link">Computer<i class="fa-solid fa-caret-down"></i></a>
                         <ul class="c-header__childList">
-                            <li><a href="#" class="c-header__itemChild">PC Gaming</a></li>
-                            <li><a href="#" class="c-header__itemChild">PC Office</a></li>
-                            <li><a href="#" class="c-header__itemChild">PC Old</a></li>
+                            <li class="c-header__childItem"><a href="#" class="c-header__childLink">PC Gaming</a></li>
+                            <li class="c-header__childItem"><a href="#" class="c-header__childLink">PC Office</a></li>
+                            <li class="c-header__childItem"><a href="#" class="c-header__childLink">PC Old</a></li>
                         </ul>
                     </li>
-                    <li class="c-header__menuItem"><a href="#">Laptop<i class="fa-solid fa-caret-down"></i></a></li>
-                    <li class="c-header__menuItem"><a href="#">Gear<i class="fa-solid fa-caret-down"></i></a></li>
-                    <li class="c-header__menuItem"><a href="#">Monitor<i class="fa-solid fa-caret-down"></i></a></li>
-                    <li class="c-header__menuItem"><a href="#">Network<i class="fa-solid fa-caret-down"></i></a></li>
-                    <li class="c-header__menuItem"><a href="#">Contact</a></li>
+                    <li class="c-header__menuItem">
+                        <a href="#" class="c-header__link">Laptop<i class="fa-solid fa-caret-down"></i></a>
+                        <ul class="c-header__childList">
+                            <li class="c-header__childItem"><a href="#" class="c-header__childLink">Laptop Gaming</a></li>
+                            <li class="c-header__childItem"><a href="#" class="c-header__childLink">Laptop Office</a></li>
+                            <li class="c-header__childItem"><a href="#" class="c-header__childLink">Laptop Old</a></li>
+                        </ul>
+                    </li>
+                    <li class="c-header__menuItem"><a href="#" class="c-header__link">Gear<i class="fa-solid fa-caret-down"></i></a></li>
+                    <li class="c-header__menuItem"><a href="#" class="c-header__link">Monitor<i class="fa-solid fa-caret-down"></i></a></li>
+                    <li class="c-header__menuItem"><a href="#" class="c-header__link">Network<i class="fa-solid fa-caret-down"></i></a></li>
+                    <li class="c-header__menuItem"><a href="#" class="c-header__link">Contact</a></li>
                 </ul>
                 <div class="c-header__phone">
-                    <span>Hotline: </span>
-                    <a href="tel:0123456789">0123456789</a>
+                    <span>Hotline: <a href="tel:0123456789">0123456789</a></span>
+
                 </div>
             </div>
         </div>
     </div>
+    <!-- end: navbar sp -->
+    <div class="c-header__nav">
+        <div class="l-container">
+            <div class="c-header__nav__content">
+                <div class="c-header__nav__btn">
+                    <i class="fa-solid fa-bars"></i>
+                </div>
+                <div class="c-header__nav__tel">
+                    <span class="c-header__phone"><span>Hotline: </span><a href="tel:1234567890">0123456789</a></span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="c-header__nav__menu">
+        <div class="c-header__nav__position">
+            <div class="l-container">
+                <ul class="c-header__nav__list">
+                    <li class="c-header__nav__item">
+                        <a href="#" class="c-header__nav__link">Home</a>
+                    </li>
+                    <li class="c-header__nav__item">
+                        <a href="#" class="c-header__nav__link">
+                            <span>Computer</span>
+                            <i class="fa-solid fa-plus"></i>
+                            <i class="fa-solid fa-minus"></i>
+                        </a>
+                        <ul class="c-header__nav__subMenu">
+                            <li class="c-header__nav__subItem">
+                                <a href="#"><span>PC Gaming</span></a>
+                            </li>
+                            <li class="c-header__nav__subItem">
+                                <a href="#"><span>PC Office</span></a>
+                            </li>
+                            <li class="c-header__nav__subItem">
+                                <a href="#"><span>PC Old</span></a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="c-header__nav__item">
+                        <a href="#" class="c-header__nav__link">
+                            <span>Laptop</span>
+                            <i class="fa-solid fa-plus"></i>
+                            <i class="fa-solid fa-minus"></i>
+                        </a>
+                        <ul class="c-header__nav__subMenu">
+                            <li class="c-header__nav__subItem">
+                                <a href="#"><span>PC Gaming</span></a>
+                            </li>
+                            <li class="c-header__nav__subItem">
+                                <a href="#"><span>PC Office</span></a>
+                            </li>
+                            <li class="c-header__nav__subItem">
+                                <a href="#"><span>PC Old</span></a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="c-header__nav__item">
+                        <a href="#" class="c-header__nav__link">
+                            <span>Gear</span>
+                            <i class="fa-solid fa-plus"></i>
+                            <i class="fa-solid fa-minus"></i>
+                        </a>
+                    </li>
+                    <li class="c-header__nav__item">
+                        <a href="#" class="c-header__nav__link">
+                            <span>Monitor</span>
+                            <i class="fa-solid fa-plus"></i>
+                            <i class="fa-solid fa-minus"></i>
+                        </a>
+                    </li>
+                    <li class="c-header__nav__item">
+                        <a href="#" class="c-header__nav__link">
+                            <span>Network</span>
+                            <i class="fa-solid fa-plus"></i>
+                            <i class="fa-solid fa-minus"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!-- end: navbar sp -->
