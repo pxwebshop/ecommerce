f620749caf15b10ad70c223d43f8a8921b50097d
dangquy <pxwebshop@gmail.com>
2022-04-14 18:01:09 +0700

    add code

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -5,20 +5,22 @@
 <main class="p-home">
     <section class="p-home__slide">
         <div class="c-slide1">
-            <div class="l-container">
-                <div class="c-slide1__content">
-                    <div class="c-slide1__text">
-                        <p class="c-slide1__bold">Big Sale Offer</p>
-                        <h1 class="c-slide1__title">The Drone has an Attractive Gift Free</h1>
-                        <p class="c-text1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                        <a href="#" class="c-btn1">
-                            <i class="fa-solid fa-cart-shopping"></i>
-                            Shop Now
-                        </a>
+            <div class="c-slide1__item">
+                <div class="l-container">
+                    <div class="c-slide1__content">
+                        <div class="c-slide1__text">
+                            <p class="c-slide1__bold">Big Sale Offer</p>
+                            <h1 class="c-slide1__title">The Drone has an Attractive Gift Free</h1>
+                            <p class="c-text1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            <a href="#" class="c-btn1">
+                                <i class="fa-solid fa-cart-shopping"></i>
+                                Shop Now
+                            </a>
+                        </div>
+                        <figure class="c-slide1__img">
+                            <img src="https://templates.envytheme.com/ejon/default/assets/img/main-slider/drone.png" alt="">
+                        </figure>
                     </div>
-                    <figure class="c-slide1__img">
-                        <img src="https://templates.envytheme.com/ejon/default/assets/img/main-slider/drone.png" alt="">
-                    </figure>
                 </div>
             </div>
         </div>
