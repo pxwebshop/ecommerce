aeb8a8032a429b061ff26a9e0baddf7d916b8289
dangquy <pxwebshop@gmail.com>
2022-04-17 22:56:20 +0700

    fix

--- public/js/front/main.js
+++ public/js/front/main.js
@@ -180,11 +180,11 @@ jQuery(function () {
     var n = 1;
     $(".c-plusMinus__counter input").val(n);
     $(".c-plusMinus__plus").on("click", function () {
-        $(".c-plusMinus__counter input").val(++n);
+        $(this).prev("input").val(++n);
     });
     $(".c-plusMinus__minus").on("click", function () {
         if (n >= 1) {
-            $(".c-plusMinus__counter input").val(--n);
+            $(this).next("input").val(--n);
         }
     });
 
