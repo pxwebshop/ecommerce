de0d3ebb14c5a1583db0e18cd77777f75804d46f
dangquy <pxwebshop@gmail.com>
2022-05-27 15:34:24 +0700

    remove

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::group(['middleware' => 'locale'], function () {
     //change language
     Route::get('change-language/{language}', 'Front\HomeController@changeLanguage')->name('user.change-language');
