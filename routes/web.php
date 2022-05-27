e14c9a53b3738318bfca687b16f505262d457bac
dangquy <pxwebshop@gmail.com>
2022-05-27 15:46:31 +0700

    add router

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,53 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Clear Cache facade value:
+Route::get('/clear-cache', function() {
+    $exitCode = Artisan::call('cache:clear');
+    return '<h1>Cache facade value cleared</h1>';
+});
+
+//Reoptimized class loader:
+Route::get('/optimize', function() {
+    $exitCode = Artisan::call('optimize');
+    return '<h1>Reoptimized class loader</h1>';
+});
+
+//Route cache:
+Route::get('/route-cache', function() {
+    $exitCode = Artisan::call('route:cache');
+    return '<h1>Routes cached</h1>';
+});
+
+//Clear Route cache:
+Route::get('/route-clear', function() {
+    $exitCode = Artisan::call('route:clear');
+    return '<h1>Route cache cleared</h1>';
+});
+
+//Clear View cache:
+Route::get('/view-clear', function() {
+    $exitCode = Artisan::call('view:clear');
+    return '<h1>View cache cleared</h1>';
+});
+
+//Clear Config cache:
+Route::get('/config-cache', function() {
+    $exitCode = Artisan::call('config:cache');
+    return '<h1>Clear Config cleared</h1>';
+});
+
+Route::get('/migrate-seed', function() {
+    // Extend maximum execution time to 3 minutes
+    set_time_limit(180);
+    Artisan::call('migrate:refresh');
+    Artisan::call('db:seed');
+    // Back to the default
+    set_time_limit(30);
+    return '<h1>Migrate --Seed</h1>';
+});
+
+
 Route::group(['middleware' => 'locale'], function () {
     //change language
     Route::get('change-language/{language}', 'Front\HomeController@changeLanguage')->name('user.change-language');
